e3a9c5371c7b100535c0166643f73dd594930ab0
William Crews <crewswh@gmail.com>
2021-04-24 15:27:19 -0400

    it work

--- assets/script.js
+++ assets/script.js
@@ -11,9 +11,6 @@ console.log(document.body.children);
 console.log("Second child of body: ")
 console.log(document.body.children[1]);
 
-// Accessing element by id
-var firstChildUl = document.getElementById("first-child-ul");
-
 var One = document.getElementById("One")
 
-document.getElementById("main").style.justifyContent = "center";
+One.style.backgroundColor = "white"
