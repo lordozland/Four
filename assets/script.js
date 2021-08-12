0e497c3d18cb47f77056f0e361c9d336bdc5b857
William Crews <crewswh@gmail.com>
2021-08-11 20:43:39 -0400

    now

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,62 @@
-// var boswer = document.getElementById('chere');
+// Question One
 
 $( "#buttOne" ).click(function() {
-    console.log("hat");
-    localStorage.setItem("duck", "duck1");
-    var duck = localStorage.getItem("duck");
-    console.log(duck);
+    console.log("hat1");
+    localStorage.setItem("duck1", "duck1");
+    var duck1 = localStorage.getItem("duck1");
+    console.log(duck1);
   });
 
 $( "#buttTwo" ).click(function() {
-    console.log("crash");
-    localStorage.setItem("falcon", "falcon1");
-    var falcon = localStorage.getItem("falcon");
-    console.log(falcon);
+    console.log("crash1");
+    localStorage.setItem("falcon1", "falcon1");
+    var falcon1 = localStorage.getItem("falcon1");
+    console.log(falcon1);
   });
 
   $( "#buttThree" ).click(function() {
-    console.log("splash");
-    localStorage.setItem("raven", "raven1");
-    var raven = localStorage.getItem("raven");
-    console.log(raven);
+    console.log("splash1");
+    localStorage.setItem("raven1", "raven1");
+    var raven1 = localStorage.getItem("raven1");
+    console.log(raven1);
   });
 
   $( "#buttFour" ).click(function() {
-    console.log("snare");
-    localStorage.setItem("eagle", "eagle1");
-    var eagle = localStorage.getItem("eagle");
-    console.log(eagle);
+    console.log("snare1");
+    localStorage.setItem("eagle1", "eagle1");
+    var eagle1 = localStorage.getItem("eagle1");
+    console.log(eagle1);
+  });
+
+
+
+  //   Questions Two  
+
+  $( "#buttFive" ).click(function() {
+    console.log("hat2");
+    localStorage.setItem("duck2", "duck2");
+    var duck2 = localStorage.getItem("duck2");
+    console.log(duck2);
+  });
+
+$( "#buttSix" ).click(function() {
+    console.log("crash2");
+    localStorage.setItem("falcon2", "falcon2");
+    var falcon2 = localStorage.getItem("falcon2");
+    console.log(falcon2);
+  });
+
+  $( "#buttSeven" ).click(function() {
+    console.log("splash2");
+    localStorage.setItem("raven2", "raven2");
+    var raven2 = localStorage.getItem("raven2");
+    console.log(raven2);
+  });
+
+  $( "#buttEight" ).click(function() {
+    console.log("snare2");
+    localStorage.setItem("eagle2", "eagle2");
+    var eagle2 = localStorage.getItem("eagle2");
+    console.log(eagle2);
   });
 
